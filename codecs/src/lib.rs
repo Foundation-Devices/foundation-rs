8ad42987bb05ae1d9075e7e4edd8469392a305a8
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2023-03-17 17:32:56 +0100

    SFT-1594: Add empty crate
    Signed-off-by: Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>

---
+++ codecs/src/lib.rs
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: © 2023 Foundation Devices, Inc. <hello@foundationdevices.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+//! # Foundation Codecs
+//!
+//! Implementations of `no_std` friendly _codecs_.
+
+#![no_std]
