9711653a352c044d0d7abea7d647715d7b2a165d
Georges Palauqui <georges@foundationdevices.com>
2023-09-12 19:37:11 +0200

    wip stratum-v1 no_std client impl

---
+++ stratum-v1/src/lib.rs
@@ -0,0 +1,8 @@
+//! Stratum v1 client.
+//!
+//! This library provides client side functions to create requests and parse responses for Stratum v1 protocol.
+#![no_std]
+
+pub mod client_not;
+pub mod client_req;
+pub mod client_resp;
