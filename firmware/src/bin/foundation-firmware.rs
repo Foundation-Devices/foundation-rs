6bac8b7af27f8b29b8b1fe20735be350fcb08b9d
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2024-05-15 18:22:56 +0200

    SFT-3631: Add flag to only verify the header.
    * src/bin/foundation-firmware.rs
    (main): Add --header-only flag.
    (print_header): New function.
    (verify_signature): New function.

--- firmware/src/bin/foundation-firmware.rs
+++ firmware/src/bin/foundation-firmware.rs
@@ -3,8 +3,8 @@
 
 use anyhow::{bail, Context, Result};
 use bitcoin_hashes::{sha256, sha256d, Hash, HashEngine};
-use clap::{command, value_parser, Arg};
-use foundation_firmware::{header, verify_signature, Information, HEADER_LEN};
+use clap::{command, value_parser, Arg, ArgAction};
+use foundation_firmware::{header, Header, Information, HEADER_LEN};
 use nom::Finish;
 use secp256k1::global::SECP256K1;
 use std::{fs, path::PathBuf};
@@ -18,6 +18,12 @@ fn main() -> Result<()> {
                 .value_parser(value_parser!(PathBuf))
                 .help("Firmware file name"),
         )
+        .arg(
+            Arg::new("header-only")
+                .long("header-only")
+                .action(ArgAction::SetTrue)
+                .help("Only validate the header of the firmware, DISABLES signature verification"),
+        )
         .get_matches();
 
     let file_name = matches.get_one::<PathBuf>("file-name").unwrap();
@@ -29,7 +35,44 @@ fn main() -> Result<()> {
         Err(_) => bail!("failed to parse firmware header"),
     };
 
+    print_header(&header);
+
     header.verify().context("header verification failed")?;
+
+    if !matches.get_flag("header-only") {
+        verify_signature(&header, &file_buf)?;
+    }
+
+    Ok(())
+}
+
+fn print_header(header: &Header) {
+    let signature1 = header.signature.signature1.serialize_compact();
+    let signature2 = header.signature.signature2.serialize_compact();
+
+    println!("Firmware:");
+    println!(
+        "{:>17}: {:#08X} ({}) ",
+        "Magic",
+        header.information.magic,
+        if header.information.magic == Information::MAGIC_COLOR {
+            "color"
+        } else {
+            "mono"
+        },
+    );
+    println!("{:>17}: {}", "Timestamp", header.information.timestamp);
+    println!("{:>17}: {}", "Date", header.information.date);
+    println!("{:>17}: {}", "Version", header.information.version);
+    println!("{:>17}: {} bytes", "Length", header.information.length);
+    println!("{:>17}: {}", "Key", header.signature.public_key1);
+    println!("{:>17}: {}", "Signature", hex::encode(signature1));
+    println!("{:>17}: {}", "Key", header.signature.public_key2);
+    println!("{:>17}: {}", "Signature", hex::encode(signature2));
+}
+
+fn verify_signature(header: &Header, file_buf: &[u8]) -> anyhow::Result<()> {
+    let header_len = usize::try_from(HEADER_LEN).unwrap();
 
     let file_hash = sha256::Hash::hash(&file_buf);
     let build_hash = sha256::Hash::hash(&file_buf[header_len..]);
@@ -54,28 +97,7 @@ fn main() -> Result<()> {
         );
     }
 
-    let signature1 = header.signature.signature1.serialize_compact();
-    let signature2 = header.signature.signature2.serialize_compact();
-
-    println!("Firmware:");
-    println!(
-        "{:>17}: {:#08X} ({}) ",
-        "Magic",
-        header.information.magic,
-        if header.information.magic == Information::MAGIC_COLOR {
-            "color"
-        } else {
-            "mono"
-        },
-    );
-    println!("{:>17}: {}", "Timestamp", header.information.timestamp);
-    println!("{:>17}: {}", "Date", header.information.date);
-    println!("{:>17}: {}", "Version", header.information.version);
-    println!("{:>17}: {} bytes", "Length", header.information.length);
-    println!("{:>17}: {}", "Key", header.signature.public_key1);
-    println!("{:>17}: {}", "Signature", hex::encode(signature1));
-    println!("{:>17}: {}", "Key", header.signature.public_key2);
-    println!("{:>17}: {}", "Signature", hex::encode(signature2));
+    println!("Validation:");
     println!("{:>17}: {}", "File Hash", file_hash);
     println!("{:>17}: {}", "Build Hash", build_hash);
     println!(
@@ -90,7 +112,7 @@ fn main() -> Result<()> {
     );
     println!();
 
-    verify_signature(SECP256K1, &header, &validation_hash, None)
+    foundation_firmware::verify_signature(&SECP256K1, &header, &validation_hash, None)
         .context("firmware signature verification failed.")?;
 
     println!("Firmware signature is valid!");
