efac99a6a253cb555c00282b448ec4cc7415038e
Georges Palauqui <georges@foundationdevices.com>
2024-08-04 19:23:00 +0200

    ci: fix REUSE

--- stratum-v1/src/client_req.rs
+++ stratum-v1/src/client_req.rs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: © 2024 Foundation Devices, Inc. <hello@foundation.xyz>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 use anyhow::{Error, Result};
 use heapless::{String, Vec};
 use hex::ToHex;
--- stratum-v1/src/lib.rs
+++ stratum-v1/src/lib.rs
@@ -1,6 +1,10 @@
+// SPDX-FileCopyrightText: © 2024 Foundation Devices, Inc. <hello@foundation.xyz>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 //! Stratum v1 client.
 //!
 //! This library provides client side functions to create requests and parse responses for Stratum v1 protocol.
+
 #![no_std]
 
 pub mod client_not;
